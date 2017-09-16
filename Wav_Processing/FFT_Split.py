03d643fc9b1c9f31e831c73d95fc32d6023309ab
Yilun Du <yilundu@mit.edu>
2017-09-16 14:52:20 -0400

    Added inverse fft

--- Wav_Processing/FFT_Split.py
+++ Wav_Processing/FFT_Split.py
@@ -7,21 +7,21 @@ from scipy.fftpack import fft
 from scipy.io import wavfile
 
 def split_and_fft(path, sample):
-	files = glob.glob(os.path.join(path, '*.wav'))
+    files = glob.glob(os.path.join(path, '*.wav'))
 
-	num = 0
-	for file in files:
+    num = 0
+    for file in files:
 
-		fs, data = wavfile.read(file) # load the data
-		
-		a = data.T[0] # this is a two channel soundtrack, I get the first track
+        fs, data = wavfile.read(file) # load the data
 
-		bitrate = 16
-		b=[(ele/2**bitrate)*2-1 for ele in a] # this is bitrate-bit track, b is now normalized on [-1,1)
+        freq = data.T[0] # this is a two channel soundtrack, I get the first track
 
-		f, t, Sxx = signal.spectrogram(np.array(b))
-		np.savez_compressed(str(num), Sxx, delimiter=',')
+        bitrate = 16
+        norm_freq =[(ele/2**bitrate)*2-1 for ele in freq]
 
-		num+=1
+        f, t, Sxx = signal.spectrogram(np.array(norm_freq))
+        np.savez_compressed(str(num), Sxx, delimiter=',')
+
+        num+=1
 
 split_and_fft(path=os.path.dirname(os.path.abspath(__file__)),sample=1000)
